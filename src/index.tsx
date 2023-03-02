b32fa3c8277735a61a898b1a415f9955f7bfbbf9
kshyun1223 <kshyun.dev@gmail.com>
2023-03-02 14:18:58 +0900

    jsx 파일을 tsx 파일로 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
+import { createRoot } from 'react-dom/client';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootNode = document.getElementById('root');
 
-ReactDOM.createRoot(rootNode).render(
+const root = createRoot(rootNode)
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
