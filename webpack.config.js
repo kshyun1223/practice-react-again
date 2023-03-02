c9149a0d612794bb33ff78d814db3a99bf25e43d
kshyun1223 <kshyun.dev@gmail.com>
2023-03-02 14:12:41 +0900

    webpack.config.js 타입스크립트 관련 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,38 @@
-/* eslint-disable no-undef */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack')
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: `${path.resolve(__dirname, './src')}/index.tsx`,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html', // 번들링 파일을 주입하여 번들링 폴더로 복사할 HTML 파일
-    }),
-  ],
+  
   devServer: { // webpack-dev-server 옵션
     static: path.resolve(__dirname, 'dist'),
     historyApiFallback: true, // 404 페이지 대신 index.html로 이동
     hot: true, // 모듈 전체를 다시 로드하지 않고 변경된 사항만 갱신
   },
+
   resolve: { // import 할 때 확장자 생략
-    extensions: ['.jsx', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader", // 바벨 로더 추가
-        test: /\.(js|jsx|ts|tsx)$/, // 적용할 대상
-        exclude: /node_modules/, // 제외할 대상
-      },
-    ],
+    extensions: ['.jsx', '.js','.ts','.tsx'],
   },
 };
