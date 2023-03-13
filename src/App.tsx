0cdc48d2b4d2138067ac477ef50b84f1dd14b79b
kshyun1223 <kshyun.dev@gmail.com>
2023-03-13 21:06:49 +0900

    styled.img

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styled from 'styled-components'
+import SquirtleSrc from '../public/asset/squirtle.png'
 
-export default function App() {
+export default function App(){
   return (
-    <h1>No CRA</h1>
-  );
+    <Container>
+      <h1>No CRA!!!</h1>
+      <Squirtle src={SquirtleSrc} />
+    </Container>
+  )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Squirtle = styled.img`
+  
+`
+
