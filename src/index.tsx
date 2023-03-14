dfc5114e5a1c748bf70b55a340220e81f3adb6b9
kshyun1223 <kshyun.dev@gmail.com>
2023-03-14 20:34:55 +0900

    router

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
-import { createRoot } from 'react-dom/client'
 import React from 'react'
-import App from './App'
+import { createRoot } from 'react-dom/client'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './components/Home'
+import About from './components/About'
 import './index.css'
+import App from './App'
 
 const rootNode = document.getElementById('root')
-
 const root = createRoot(rootNode)
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
